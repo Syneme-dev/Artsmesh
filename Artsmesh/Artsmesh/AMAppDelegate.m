05608719ce7c3d882b682f5b9ca42219720e26ec
Sky JIA <jiachao@farefore.com>
2013-12-30 10:05:13 +0800

    Init Artsmesh Xcode project.

---
+++ Artsmesh/Artsmesh/AMAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AMAppDelegate.m
+//  Artsmesh
+//
+//  Created by Sky JIA on 12/30/13.
+//  Copyright (c) 2013 Sky JIA. All rights reserved.
+//
+
+#import "AMAppDelegate.h"
+
+@implementation AMAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Artsmesh/Artsmesh/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Artsmesh
+//
+//  Created by Sky JIA on 12/30/13.
+//  Copyright (c) 2013 Sky JIA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}

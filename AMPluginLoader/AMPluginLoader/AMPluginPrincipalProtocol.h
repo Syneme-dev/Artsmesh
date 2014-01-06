ea89874aba8df41d61c05211ec338e8d2db5da99
Sky JIA <jiachao@farefore.com>
2014-01-06 15:15:15 +0800

    Add principal class protocol.

---
+++ AMPluginLoader/AMPluginLoader/AMPluginPrincipalProtocol.h
@@ -0,0 +1,13 @@
+//
+//  AMPluginPrincipalProtocol.h
+//  AMPluginLoader
+//
+//  Created by Sky JIA on 1/6/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AMPluginPrincipalProtocol <NSObject>
+
+@end

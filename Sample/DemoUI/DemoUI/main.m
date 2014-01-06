d747eb805c665385d6f8208be251def4e4384c01
Sky JIA <jiachao@farefore.com>
2014-01-06 15:28:27 +0800

    Reorganise the projects in workspace and add DemoUI project.

---
+++ Sample/DemoUI/DemoUI/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DemoUI
+//
+//  Created by Sky JIA on 1/6/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}

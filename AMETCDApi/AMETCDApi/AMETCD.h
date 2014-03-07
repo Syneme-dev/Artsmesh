ac3527c8b2db543669cddb70c90a702eb1d6c2b5
WangWei <mkitmyway@gmail.com>
2014-03-07 14:39:20 +0800

    add comment

--- AMETCDApi/AMETCDApi/AMETCD.h
+++ AMETCDApi/AMETCDApi/AMETCD.h
@@ -17,38 +17,63 @@
 @property int clientPort;
 @property NSString* nodeName;
 
-//If we don't assign parameters, the command will be:
-//etcd  -peer-addr 192.168.1.101:7001
-//      -addr 192.168.1.101:4001
-//      -data-dir machineName
-//      -name machienName
+
+//the default ip is host ip,
+//client port:4001
+//server port: 7001
 -(id)init;
 
+//now assume that etcd is in folder /usr/bin/etcd
+//will be modified when we designed the physic file path
 -(BOOL)startETCD;
 
+//will kill and running etcd instance and run ours
 -(void)stopETCD;
 
+//get a key command
+//the result is in AMETCDResult, see the AMETCDResult to
+//know the result structure
 -(AMETCDResult*)getKey:(NSString*)key;
 
+//set a key
 -(AMETCDResult*)setKey:(NSString*)key
              withValue:(NSString*)value;
 
+//wait the key changing until timeout
+//the index_in is the global index we want to wait
+//the index_out tell us the latest index of related to the
+//key so if we can use 2 as the index_in, if the key exist,
+//it will return, then we get the actual index and wait the
+//actual index+1. then we will never miss any change of the
+//key
 -(AMETCDResult*)watchKey:(NSString*)key
                    fromIndex:(int)index_in
                 acturalIndex:(int*)index_out
                      timeout:(int)seconds;
 
+//delete a key
 -(AMETCDResult*)deleteKey: (NSString*) Key;
 
+//create a dir, if the dir is exist, the return
+//resule errCode will not be 0
 -(AMETCDResult*)createDir:(NSString*)dirPath;
 
+//delete a dir. if the recursive is NO, only
+//delete empty dir, if the recursive is YES
+//will delete any dir, except the root dir
 -(AMETCDResult*)deleteDir:(NSString*)dirPath
                 recursive:(BOOL)b;
 
+//list the Dir
+//if the recursive is NO, will only get the current
+//content, if the recursive is YES, will also get the content
+//in subDir
 -(AMETCDResult*)listDir:(NSString*)dirPath
               recursive:(BOOL)bRecursive;
 
 
+//watch a dir
+//the index meaning is the same as watch a key
 -(AMETCDResult*)watchDir:(NSString*)dirPath
                fromIndex:(int)index_in
             acturalIndex:(int*)index_out

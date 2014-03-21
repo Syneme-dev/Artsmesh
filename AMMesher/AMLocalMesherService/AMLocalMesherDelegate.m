8e061f16b2269e2e0ca5e6c8a28ce0487cb58774
Wang Wei <mkitmyway@gmail.com>
2014-03-21 22:02:49 +0800

    go to sleep

--- AMMesher/AMLocalMesherService/AMLocalMesherDelegate.m
+++ AMMesher/AMLocalMesherService/AMLocalMesherDelegate.m
@@ -11,13 +11,22 @@
 #import "AMLocalMesherService.h"
 
 @implementation AMLocalMesherDelegate
+{
+    AMLocalMesherService* _service;
+}
+
 
 - (BOOL)listener:(NSXPCListener *)listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection
 {
+    if (_service == nil)
+    {
+        _service = [[AMLocalMesherService alloc] init];
+    }
+    
     NSXPCInterface* interface = [NSXPCInterface interfaceWithProtocol:@protocol(AMLocalMesherInterface)];
     
     newConnection.exportedInterface = interface;
-    newConnection.exportedObject = [[AMLocalMesherService alloc] init];
+    newConnection.exportedObject = _service;
     
     [newConnection resume];
     

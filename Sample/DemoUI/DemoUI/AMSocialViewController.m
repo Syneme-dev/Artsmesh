a21ccb909cbc7eee07e6e5f0765e2f7854e47dc5
xujianrj <keys203@gmail.com>
2014-05-20 16:07:33 +0800

    enable to using status net preference to login social panel.

--- Sample/DemoUI/DemoUI/AMSocialViewController.m
+++ Sample/DemoUI/DemoUI/AMSocialViewController.m
@@ -8,8 +8,15 @@
 
 #import "AMSocialViewController.h"
 #import <Cocoa/Cocoa.h>
+
+#import "AMPreferenceManager/AMPreferenceManager.h"
 #import <WebKit/WebKit.h>
 @interface AMSocialViewController ()
+{
+    Boolean isLogin;
+    NSString* statusNetURL;
+}
+
 
 @end
 
@@ -26,43 +33,88 @@
 
 
 -(void)loadPage{
-    NSURL *baseURL = [NSURL URLWithString:@"http://artsmesh.io/main/login?fromMac=true"];
+    isLogin=false;
+     NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+     statusNetURL= [defaults stringForKey:Preference_Key_StatusNet_URL];
+    NSURL *loginURL = [NSURL URLWithString:
+                      [NSString stringWithFormat:@"%@/main/login?fromMac=true",statusNetURL ]];
+    [self.socialWebTab.mainFrame loadRequest:
+     [NSURLRequest requestWithURL:loginURL]];
+}
+
+-(void)gotoUsersPage{
+    NSURL *baseURL =
+    [NSURL URLWithString:
+     [NSString stringWithFormat:@"%@/directory/users?fromMac=true",statusNetURL ]];
     [self.socialWebTab.mainFrame loadRequest:
      [NSURLRequest requestWithURL:baseURL]];
+}
 
+-(void)gotoGroupsPage{
+    NSURL *baseURL =
+    [NSURL URLWithString:
+     [NSString stringWithFormat:@"%@/groups?fromMac=true",statusNetURL ]];
+    [self.socialWebTab.mainFrame loadRequest:
+     [NSURLRequest requestWithURL:baseURL]];
 }
 
 
 - (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame
 {
+    NSString *url= sender.mainFrameURL;
     self.socialWebTab.preferences.userStyleSheetEnabled = YES;
     NSString *path= [[NSBundle mainBundle] bundlePath];
-    path=[path stringByAppendingString:@"/Contents/Resources/web.css"];
+    if([url isEqual:@"http://artsmesh.io/xujian?fromMac=true"])
+    {
+        path=[path stringByAppendingString:@"/Contents/Resources/info.css"];
+    }
+    else{
+        path=[path stringByAppendingString:@"/Contents/Resources/web.css"];
+    }
     self.socialWebTab.preferences.userStyleSheetLocation = [NSURL fileURLWithPath:path];
-    [self login:frame];
+
+    if(!isLogin)
+    {    [self login:frame];}
 }
 
 -(void)login:(WebFrame *)frame{
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    NSString* username = [defaults stringForKey:Preference_Key_StatusNet_UserName];
+    NSString* password = [defaults stringForKey:Preference_Key_StatusNet_Password];
+    NSString *loginJs=[NSString stringWithFormat:@"$('#nickname').val('%@');$('#password').val('%@');$('#submit').click();",username,password ];
           [frame.webView stringByEvaluatingJavaScriptFromString:
-           @"$('#nickname').val('xujian');$('#password').val('p@ssw0rd');$('#submit').click();" ];
+           loginJs];
+    isLogin=TRUE;
 }
 
 
 - (IBAction)onFOAFTabClick:(id)sender{
-    [self.tabs selectTabViewItemWithIdentifier:@"1"];
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    NSString* username = [defaults stringForKey:Preference_Key_StatusNet_UserName];
+    NSURL *baseURL =
+    [NSURL URLWithString:
+    [NSString stringWithFormat:@"%@/%@?fromMac=true",statusNetURL,username ]];
+    [self.socialWebTab.mainFrame loadRequest:
+     [NSURLRequest requestWithURL:baseURL]];
 }
 
 
 
 - (IBAction)onBlogTabClick:(id)sender{
-[self.tabs selectTabViewItemWithIdentifier:@"2"];
+    NSURL *baseURL =
+    [NSURL URLWithString:
+     [NSString stringWithFormat:@"%@?fromMac=true",statusNetURL ]];
+    [self.socialWebTab.mainFrame loadRequest:
+     [NSURLRequest requestWithURL:baseURL]];
+
 }
 
 - (IBAction)onUpButtonClick:(id)sender{
-
+     [self gotoUsersPage];
 }
 
 - (IBAction)onAddFieldButtonClick:(id)sender{
+     [self gotoGroupsPage];
 
 }
 

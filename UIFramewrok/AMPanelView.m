b23aaf0485089aecfb20084d4a1fd99f80d0f249
lattesir <gangofn@gmail.com>
2014-05-21 14:26:49 +0800

    aaaa

--- UIFramewrok/AMPanelView.m
+++ UIFramewrok/AMPanelView.m
@@ -9,20 +9,74 @@
 #import "AMPanelView.h"
 
 @implementation AMPanelView
+{
+    NSRect _knobRect;
+    NSColor *_knobColor;
+    NSPoint _mouseDownLocation;
+}
 
 - (id)initWithFrame:(NSRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code here.
+
     }
     return self;
 }
 
+- (void)awakeFromNib
+{
+//
+//    _knobColor = [NSColor colorWithCalibratedRed:(46)/255.0f
+//                                           green:(58)/255.0f
+//                                            blue:(75)/255.0f
+//                                           alpha:1.0f];
+    _knobColor = [NSColor redColor];
+}
+
+- (void)mouseDown:(NSEvent *)theEvent
+{
+    [super mouseDown:theEvent];
+    _mouseDownLocation = [self convertPoint:[theEvent locationInWindow]
+                                   fromView:nil];
+}
+
+- (NSRect)getKnobRect
+{
+    NSSize size = self.bounds.size;
+    return NSMakeRect(size.width - 16, size.height - 16, 16, 16);
+}
+
+- (void)mouseDragged:(NSEvent *)theEvent
+{
+    if (NSPointInRect(_mouseDownLocation, [self getKnobRect])) {
+        NSPoint mouseLocation = [self convertPoint:[theEvent locationInWindow]
+                                          fromView:nil];
+        CGFloat deltaX = mouseLocation.x - _mouseDownLocation.x;
+        CGFloat deltaY = mouseLocation.y - _mouseDownLocation.y;
+//        _knobRect = NSOffsetRect(_knobRect, deltaX, deltaY);
+        NSSize frameSize = self.frame.size;
+        frameSize.width += deltaX;
+        frameSize.height += deltaY;
+        [self setFrameSize:frameSize];
+    } else {
+        [super mouseDragged:theEvent];
+    }
+}
+
+- (BOOL)isFlipped
+{
+    return YES;
+}
+
 - (void)drawRect:(NSRect)dirtyRect
 {
-    [[NSColor colorWithWhite:0.22 alpha:1.0] set];
-    [NSBezierPath fillRect:self.bounds];
+//    [[NSColor colorWithWhite:0.22 alpha:1.0] set];
+//    [NSBezierPath fillRect:self.bounds];
+    [_knobColor set];
+    NSRect rect = [self getKnobRect];
+    [NSBezierPath fillRect:rect];
 }
+
 
 @end

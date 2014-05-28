6e54161b408e08d9c5ea937602d4ee2a0e2c2d0d
lattesir <gangofn@gmail.com>
2014-05-29 07:37:39 +0800

    bug fix

--- AMMesher/AMMesher/AMHeartBeat.m
+++ AMMesher/AMMesher/AMHeartBeat.m
@@ -63,69 +63,71 @@ NSString * const AMHeartBeatErrorDomain = @"AMHeartBeatErrorDomain";
 {
     NSTimeInterval sleepTime = 0;
     while (!self.isCancelled) {
-        if (sleepTime > 0)
-            [NSThread sleepForTimeInterval:sleepTime];
-        
-        if (!self.delegate) {
-            sleepTime = self.timeInterval;
-            continue;
-        }
-        
-        NSData *data = nil;
-        if ([self.delegate respondsToSelector:@selector(heartBeatData)])
-            data = [self.delegate heartBeatData];
-        if (data == nil || data.length == 0) {
-            sleepTime = self.timeInterval;
-            continue;
-        }
-        
-        int sockfd = socket(_family, SOCK_DGRAM, IPPROTO_UDP);
-        if (sockfd == -1) {
-            [self reportError:AMHeartBeatErrorCreateSocketFailed];
-            sleepTime = self.timeInterval;
-            continue;
-        }
-        
-        NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
-        
-        struct timeval timeout;
-        timeout.tv_sec = self.receiveTimeout;
-        timeout.tv_usec = (self.receiveTimeout - timeout.tv_sec) * 1000;
-        if (setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,&timeout,
-                       sizeof(timeout)) == -1) {
-            [self reportError:AMHeartBeatErrorSetSocketTimoutFailed];
-            goto cleanup;
-        }
-        
-        ssize_t nbytes = -1;
-        nbytes = sendto(sockfd, data.bytes, data.length, 0, _serverAddress.bytes,
-                        (socklen_t)_serverAddress.length);
-        if (nbytes == -1) {
-            [self reportError:AMHeartBeatErrorSendDataFailed];
-            goto cleanup;
-        }
-        if ([self.delegate respondsToSelector:@selector(heartBeat:didSendData:)])
-            [self.delegate heartBeat:self didSendData:data];
-        data = nil;
-        
-        char rcvbuf[AMHeartBeatMaxPackageSize];
-        socklen_t addrlen = (socklen_t)_serverAddress.length;
-        nbytes = recvfrom(sockfd, rcvbuf, sizeof(rcvbuf), 0,
-                          (struct sockaddr *)_serverAddress.bytes, &addrlen);
-        if (nbytes == -1) {
-            int errorCode = (errno == ETIMEDOUT) ?
+        @autoreleasepool {
+            if (sleepTime > 0)
+                [NSThread sleepForTimeInterval:sleepTime];
+            
+            if (!self.delegate) {
+                sleepTime = self.timeInterval;
+                continue;
+            }
+            
+            NSData *data = nil;
+            if ([self.delegate respondsToSelector:@selector(heartBeatData)])
+                data = [self.delegate heartBeatData];
+            if (data == nil || data.length == 0) {
+                sleepTime = self.timeInterval;
+                continue;
+            }
+            
+            int sockfd = socket(_family, SOCK_DGRAM, IPPROTO_UDP);
+            if (sockfd == -1) {
+                [self reportError:AMHeartBeatErrorCreateSocketFailed];
+                sleepTime = self.timeInterval;
+                continue;
+            }
+            
+            NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+            
+            struct timeval timeout;
+            timeout.tv_sec = self.receiveTimeout;
+            timeout.tv_usec = (self.receiveTimeout - timeout.tv_sec) * 1000;
+            if (setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,&timeout,
+                           sizeof(timeout)) == -1) {
+                [self reportError:AMHeartBeatErrorSetSocketTimoutFailed];
+                goto cleanup;
+            }
+            
+            ssize_t nbytes = -1;
+            nbytes = sendto(sockfd, data.bytes, data.length, 0, _serverAddress.bytes,
+                            (socklen_t)_serverAddress.length);
+            if (nbytes == -1) {
+                [self reportError:AMHeartBeatErrorSendDataFailed];
+                goto cleanup;
+            }
+            if ([self.delegate respondsToSelector:@selector(heartBeat:didSendData:)])
+                [self.delegate heartBeat:self didSendData:data];
+            data = nil;
+            
+            char rcvbuf[AMHeartBeatMaxPackageSize];
+            socklen_t addrlen = (socklen_t)_serverAddress.length;
+            nbytes = recvfrom(sockfd, rcvbuf, sizeof(rcvbuf), 0,
+                              (struct sockaddr *)_serverAddress.bytes, &addrlen);
+            if (nbytes == -1) {
+                int errorCode = (errno == ETIMEDOUT) ?
                 AMHeartBeatErrorReceiveTimeout : AMHeartBeatErrorReceiveDataFailed;
-            [self reportError:errorCode];
-            goto cleanup;
+                [self reportError:errorCode];
+                goto cleanup;
+            }
+            if ([self.delegate respondsToSelector:@selector(heartBeat:didReceiveData:)])
+                [self.delegate heartBeat:self
+                          didReceiveData:[NSData dataWithBytes:rcvbuf length:nbytes]];
+            
+        cleanup:
+            close(sockfd);
+            NSTimeInterval now = [NSDate timeIntervalSinceReferenceDate];
+            sleepTime = self.timeInterval - (now - startTime);
         }
-        if ([self.delegate respondsToSelector:@selector(heartBeat:didReceiveData:)])
-            [self.delegate heartBeat:self
-                         didReceiveData:[NSData dataWithBytes:rcvbuf length:nbytes]];
-      
-cleanup:
-        close(sockfd);
-        NSTimeInterval now = [NSDate timeIntervalSinceReferenceDate];
-        sleepTime = self.timeInterval - (now - startTime);
     }
 }
 

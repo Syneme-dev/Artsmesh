5de554e4449f2d3ef11ee978c5129a01e6af1ead
lattesir <gangofn@gmail.com>
2014-07-17 11:34:29 +0800

    add border

---
+++ UIFramewrok/AMBorderView.h
@@ -0,0 +1,18 @@
+//
+//  AMBorderView.h
+//  DemoUI
+//
+//  Created by lattesir on 7/17/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define BORDER_THICKNESS    10
+
+@interface AMBorderView : NSView
+
+// designated initializer
+- (id)initWithView:(NSView *)view;
+
+@end

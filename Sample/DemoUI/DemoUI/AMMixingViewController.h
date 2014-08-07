67f190ca53a486dd24c9b0e0717f85014222ff7e
xujianrj <keys203@gmail.com>
2014-08-07 10:53:12 +0800

    enable tab for mixing

--- Sample/DemoUI/DemoUI/AMMixingViewController.h
+++ Sample/DemoUI/DemoUI/AMMixingViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AMTabPanelViewController.h"
+#import <UIFramework/AMBlueButton.h>
 
-@interface AMMixingViewController : NSViewController
+@interface AMMixingViewController : AMTabPanelViewController
+@property (strong) IBOutlet AMBlueButton *audioTab;
+@property (strong) IBOutlet AMBlueButton *videoTab;
+@property (strong) IBOutlet AMBlueButton *outputTab;
+- (IBAction)onAudioTabClick:(id)sender;
+- (IBAction)onVideoTabClick:(id)sender;
+- (IBAction)onOutputTabClick:(id)sender;
+@property (strong) IBOutlet NSTabView *tabs;
 
 @end

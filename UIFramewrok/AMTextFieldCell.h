cc8bac0d02ffdb2bbc32dc0f4152b0e69ca71989
lattesir <gangofn@gmail.com>
2014-08-08 09:01:40 +0800

    change ui attributes of selected text

---
+++ UIFramewrok/AMTextFieldCell.h
@@ -0,0 +1,13 @@
+//
+//  AMTextFieldCell.h
+//  UIFramework
+//
+//  Created by lattesir on 8/7/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMTextFieldCell : NSTextFieldCell
+
+@end

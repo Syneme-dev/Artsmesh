5afff5e36f4c59b95dc00b085fed5d38e2864ba5
WangWei <mkitmyway@gmail.com>
2014-08-24 11:37:15 +0800

    dd config file

---
+++ AMAudio/AMAudio/AMJackConfigs.h
@@ -0,0 +1,13 @@
+//
+//  AMJackConfigs.h
+//  AMAudio
+//
+//  Created by 王 为 on 8/22/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMJackConfigs : NSObject
+
+@end
---
+++ AMAudio/AMAudio/AMJackConfigs.m
@@ -0,0 +1,13 @@
+//
+//  AMJackConfigs.m
+//  AMAudio
+//
+//  Created by 王 为 on 8/22/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import "AMJackConfigs.h"
+
+@implementation AMJackConfigs
+
+@end

21a3271124d0604bc3a4f11e5e49240a040970a1
Wang Wei <mkitmyway@gmail.com>
2014-08-28 14:30:37 +0800

    remove warnings

--- AMStatusNet/AMStatusNet/AMStatusNetModule.h
+++ AMStatusNet/AMStatusNet/AMStatusNetModule.h
@@ -15,8 +15,8 @@
                  withUserName:(NSString*)username
                  withPassword:(NSString*)password;
 
--(void)getGroupsOnStatusNet:(NSString*)url
-            completionBlock:(void(^)(NSData*, NSError*))completionBlock;
+//-(void)getGroupsOnStatusNet:(NSString*)url
+//            completionBlock:(void(^)(NSData*, NSError*))completionBlock;
 
 
 @end

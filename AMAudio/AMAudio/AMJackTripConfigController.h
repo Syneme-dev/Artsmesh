ce03b14d4f92e725a662bb6e2c006cecd0279858
Wang Wei <mkitmyway@gmail.com>
2014-09-05 23:12:32 +0800

    add jacktrip popover UI

---
+++ AMAudio/AMAudio/AMJackTripConfigController.h
@@ -0,0 +1,13 @@
+//
+//  AMJackTripConfigController.h
+//  AMAudio
+//
+//  Created by Wei Wang on 9/5/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMJackTripConfigController : NSViewController
+
+@end
---
+++ AMAudio/AMAudio/AMJackTripConfigController.m
@@ -0,0 +1,26 @@
+//
+//  AMJackTripConfigController.m
+//  AMAudio
+//
+//  Created by Wei Wang on 9/5/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import "AMJackTripConfigController.h"
+
+@interface AMJackTripConfigController ()
+
+@end
+
+@implementation AMJackTripConfigController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end

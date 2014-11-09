787eae1777aaa7e7cd0f13a3a15bac71f0de015b
mkitmyway <mkitmyway@gmail.com>
2014-11-09 12:41:44 +0800

    add mixer UI

---
+++ AMAudio/AMAudio/AMAudioMixerViewController.m
@@ -0,0 +1,22 @@
+//
+//  AMAudioMixerView.m
+//  AMAudio
+//
+//  Created by wangwei on 9/11/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import "AMAudioMixerView.h"
+
+@interface AMAudioMixerView ()
+
+@end
+
+@implementation AMAudioMixerView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

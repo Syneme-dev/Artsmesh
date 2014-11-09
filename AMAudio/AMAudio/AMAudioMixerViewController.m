7c6f5a5f95476c638c942f31eb047a520031037a
mkitmyway <mkitmyway@gmail.com>
2014-11-09 22:40:39 +0800

    add start button

--- AMAudio/AMAudio/AMAudioMixerViewController.m
+++ AMAudio/AMAudio/AMAudioMixerViewController.m
@@ -9,14 +9,32 @@
 #import "AMAudioMixerViewController.h"
 
 @interface AMAudioMixerViewController ()
+@property (weak) IBOutlet NSButton *startMixerBtn;
+@property (weak) IBOutlet NSTextField *channelPairCount;
 
 @end
 
 @implementation AMAudioMixerViewController
+{
+    BOOL _mixerStarted;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
 }
+
+- (IBAction)startMixer:(NSButton *)sender
+{
+    if (_mixerStarted){
+        self.startMixerBtn.title = @"Start Mixer" ;
+        _mixerStarted = NO;
+    }else{
+        self.startMixerBtn.title = @"Stop Mixer" ;
+        _mixerStarted = YES;
+    }
+}
+
+
 
 @end

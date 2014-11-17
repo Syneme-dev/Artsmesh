a905b86a2cf962860bc6c959be5d1c49447c49d5
Wang Wei <mkitmyway@gmail.com>
2014-11-17 13:51:40 +0800

    add am vide project

---
+++ AMVideo/AMVideo/AMVideo.h
@@ -0,0 +1,19 @@
+//
+//  AMVideo.h
+//  AMVideo
+//
+//  Created by 王为 on 17/11/14.
+//  Copyright (c) 2014 王为. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AMVideo.
+FOUNDATION_EXPORT double AMVideoVersionNumber;
+
+//! Project version string for AMVideo.
+FOUNDATION_EXPORT const unsigned char AMVideoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMVideo/PublicHeader.h>
+
+

5363f2a32a423a8337aea2d5b08853a7ac12e36b
whiskyzed <whiskyzed@gmail.com>
2014-12-07 14:19:03 +0800

    Add tearOff view controller

---
+++ Sample/DemoUI/DemoUI/AMSyphonTearOffController.m
@@ -0,0 +1,22 @@
+//
+//  AMSyphonTearOffController.m
+//  DemoUI
+//
+//  Created by whiskyzed on 12/7/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import "AMSyphonTearOffController.h"
+
+@interface AMSyphonTearOffController ()
+
+@end
+
+@implementation AMSyphonTearOffController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

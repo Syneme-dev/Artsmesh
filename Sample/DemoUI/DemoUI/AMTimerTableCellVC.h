49da2b38524ca2a9e97328d35b430f3843ec1aef
lattesir <gangofn@gmail.com>
2014-12-29 13:15:20 +0800

    update UI of timer panel

---
+++ Sample/DemoUI/DemoUI/AMTimerTableCellVC.h
@@ -0,0 +1,13 @@
+//
+//  AMTimerTableCellVC.h
+//  DemoUI
+//
+//  Created by GaoMing on 12/29/14.
+//  Copyright (c) 2014 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMTimerTableCellVC : NSViewController
+
+@end

870e7dc82398033c366b1e7bdfb12f18018180a4
xujianrj <keys203@gmail.com>
2015-01-11 10:16:08 +0800

    update social panel

--- Sample/DemoUI/DemoUI/AMMainWindowController.m
+++ Sample/DemoUI/DemoUI/AMMainWindowController.m
@@ -551,7 +551,7 @@
 }
 
 - (AMPanelViewController *)loadFOAFPanel:(NSString *)panelId relatedView:(NSView*)view{
-    AMPanelViewController *panelViewController = [self createPanel:panelId withTitle:@"SOCIAL" width:UI_defaultPanelWidth* 2.5 height:UI_defaultPanelHeight  relatedView:view];
+    AMPanelViewController *panelViewController = [self createPanel:panelId withTitle:@"SOCIAL" width:UI_defaultPanelWidth* 3.5 height:UI_defaultPanelHeight  relatedView:view];
     AMPanelView *panelView = (AMPanelView *) panelViewController.view;
     NSSize panelSize = NSMakeSize(UI_defaultPanelWidth* 2, UI_defaultPanelHeight);
     panelView.minSizeConstraint = panelSize;

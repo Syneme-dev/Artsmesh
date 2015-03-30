259a6858f7da34a329276860cfad217ac6209544
whiskyzed <whiskyzed@gmail.com>
2015-03-30 10:40:02 +0800

    f

--- Sample/DemoUI/DemoUI/AMNowBarView.h
+++ Sample/DemoUI/DemoUI/AMNowBarView.h
@@ -8,6 +8,10 @@
 
 #import <Cocoa/Cocoa.h>
 
+//The AMNowBarView is the line in foreground.
+//The frame of AMNowBarView set height as the same as the background view.
+// And only need to set 4~7 width
+
 @interface AMNowBarView : NSView
 
 @end
--- Sample/DemoUI/DemoUI/AMNowBarView.m
+++ Sample/DemoUI/DemoUI/AMNowBarView.m
@@ -12,44 +12,35 @@
 
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
+
     
-/*
-    NSRect  bounds      = [self bounds];
-    NSPoint nowBarStart = NSMakePoint(bounds.size.width / 3, 0);
-    NSPoint nowBarEnd   = NSMakePoint(bounds.size.width / 3, bounds.size.height);
-    NSBezierPath* path       = [NSBezierPath bezierPath];
-    [path setLineWidth:3];
-    [path moveToPoint:nowBarStart];
-    [path lineToPoint:nowBarEnd];     
-    [path fill];
-    */
-    // To draw two triangles both in the front and below end
     CGFloat width  = [self bounds].size.width;
     CGFloat height = [self bounds].size.height;
     
     NSColor *fgColor = [NSColor colorWithCalibratedRed:1 green:1 blue:1 alpha:0.5];
     [fgColor set];
     
+    //Be careful about the midBar height and triangle height.
+    //If the midBar overlap the triangle,the overlapping part is different from other.
     NSBezierPath* midBar = [NSBezierPath bezierPath];
     [midBar setLineWidth:2];
     [midBar moveToPoint:NSMakePoint(width/2, height-width+1)];
     [midBar lineToPoint:NSMakePoint(width/2, width)];
     [midBar stroke];
     
-    NSBezierPath* upperTriangle = [NSBezierPath bezierPath];
-    [upperTriangle moveToPoint:NSMakePoint(0, height)];
-    [upperTriangle lineToPoint:NSMakePoint(width, height)];
-    [upperTriangle lineToPoint:NSMakePoint(width/2, height-width)];
-    [upperTriangle fill];
-   
     
-    NSBezierPath* lowerTriangle = [NSBezierPath bezierPath];
-    [lowerTriangle moveToPoint:NSMakePoint(0, 0)];
-    [lowerTriangle lineToPoint:NSMakePoint(width, 0)];
-    [lowerTriangle lineToPoint:NSMakePoint(width/2, width+1)];
-    [lowerTriangle fill];
-
-//    NSRectFill(self.bounds);
+    // To draw two triangles both in the up and down end.
+    NSBezierPath* topTriangle = [NSBezierPath bezierPath];
+    [topTriangle moveToPoint:NSMakePoint(0, height)];
+    [topTriangle lineToPoint:NSMakePoint(width, height)];
+    [topTriangle lineToPoint:NSMakePoint(width/2, height-width)];
+    [topTriangle fill];
+   
+    NSBezierPath* bottomTriangle = [NSBezierPath bezierPath];
+    [bottomTriangle moveToPoint:NSMakePoint(0, 0)];
+    [bottomTriangle lineToPoint:NSMakePoint(width, 0)];
+    [bottomTriangle lineToPoint:NSMakePoint(width/2, width+1)];
+    [bottomTriangle fill];
 }
 
 

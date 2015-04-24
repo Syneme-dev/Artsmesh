ecff36ab8c15d5ab493d849281e82e114760164f
whiskyzed <whiskyzed@gmail.com>
2015-04-24 17:26:04 +0800

    add group function

--- Sample/DemoUI/DemoUI/AMClockTabCellVC.m
+++ Sample/DemoUI/DemoUI/AMClockTabCellVC.m
@@ -7,16 +7,42 @@
 //
 
 #import "AMClockTabCellVC.h"
+#import "UIFramework/AMPopUpview.h"
+#import "AMCoreData/AMCoreData.h"
+#import "AMCoreData/AMLiveGroup.h"
+#import "AMCoreData/AMLiveUser.h"
 
-@interface AMClockTabCellVC ()
 
+@interface AMClockTabCellVC ()<AMPopUpViewDelegeate>
+@property (weak) IBOutlet   AMPopUpView*    groupPopup;
+@property (nonatomic)       NSArray*        groups;
+@property (nonatomic)       BOOL            isLocalGroup;
 @end
 
 @implementation AMClockTabCellVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.groupPopup.delegate = self;
     // Do view setup here.
+}
+
+-(void) popupViewWillPopup:(AMPopUpView *)sender
+{
+    if ([sender isEqual:self.groupPopup]) {
+        self.groups = [[AMCoreData shareInstance] myMergedGroupsInFlat];
+        self.isLocalGroup = NO;
+        
+        if (!self.groups) {
+            self.groups = @[ [[AMCoreData shareInstance] myLocalLiveGroup] ];
+            self.isLocalGroup = YES;
+        }
+        [self.groupPopup removeAllItems];
+        
+        NSMutableArray *groupNames = [NSMutableArray array];
+        [groupNames addObjectsFromArray:[self.groups valueForKeyPath:@"groupName"]];
+        [self.groupPopup addItemsWithTitles:groupNames];
+    }
 }
 
 @end

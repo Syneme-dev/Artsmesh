a5ae466dead02999a1e33c165e61c4d90bb3019a
Brad Phillips <brad@bradphillips.org>
2015-05-20 14:36:00 -0500

    Update font and sizing for Group Overlay Description field.

--- Sample/DemoUI/DemoUI/GroupPanel/AMGroupDetailsViewController.m
+++ Sample/DemoUI/DemoUI/GroupPanel/AMGroupDetailsViewController.m
@@ -55,7 +55,9 @@
     
     if (![self.group.description isEqualToString:@""]) {
         
-        NSFont* textViewFont =  [NSFont fontWithName: @"FoundryMonoline" size: 13];
+        NSFontManager *fontManager = [NSFontManager sharedFontManager];
+        //NSFont* textViewFont =  [NSFont fontWithName: @"FoundryMonoline" size: 13];
+        NSFont *textViewFont = [fontManager fontWithFamily:@"FoundryMonoline" traits:NSBoldFontMask weight:0 size:13];
         NSDictionary* attr = @{NSForegroundColorAttributeName: [NSColor whiteColor],
                                NSFontAttributeName:textViewFont};
         NSAttributedString* attrStr = [[NSAttributedString alloc] initWithString:self.group.description attributes:attr];

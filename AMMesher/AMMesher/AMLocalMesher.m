cfc5fd30c880ce4e96fe1fabbcfe3c661f4f968d
Brad Phillips <brad@bradphillips.org>
2015-05-21 10:22:21 -0500

    Add hostname check back in to Local Server connection process.

--- AMMesher/AMMesher/AMLocalMesher.m
+++ AMMesher/AMMesher/AMLocalMesher.m
@@ -158,21 +158,12 @@
         lsIps = [config localServerIpv4s];
     }
     
-    /**
     if (_retryCount == 0) {
         _tryLocalServerAddr = config.localServerHost.name;
         
     }else if(_retryCount < [lsIps count] + 1){
         _tryLocalServerAddr = [lsIps objectAtIndex:_retryCount - 1];
         
-    }else{
-        [[NSNotificationCenter defaultCenter] postNotificationName:AM_LOCAL_SERVER_CONNECTION_ERROR object:nil];
-        return;
-    }
-    **/
-    
-    if(_retryCount < [lsIps count]) {
-        _tryLocalServerAddr = [lsIps objectAtIndex:_retryCount];
     }else{
         [[NSNotificationCenter defaultCenter] postNotificationName:AM_LOCAL_SERVER_CONNECTION_ERROR object:nil];
         return;

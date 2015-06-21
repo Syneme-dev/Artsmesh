86704759cf92e33c501c4ee4c7936c774501c3a5
xujianrj <keys203@gmail.com>
2015-06-21 15:02:48 +0800

    enable to horizontal scroll over the sub scroll panel

---
+++ Sample/DemoUI/DemoUI/AMVerticalScrollView.h
@@ -0,0 +1,13 @@
+//
+//  AMVerticalScrollView
+//  Artsmesh
+//
+//  Created by KeysXu on 6/21/15.
+//  Copyright (c) 2015 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMVerticalScrollView : NSScrollView
+
+@end

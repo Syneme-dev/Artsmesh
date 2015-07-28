d921b640849ad552ad8fba480b0b6c086df434ba
Brad Phillips <brad@bradphillips.org>
2015-07-28 00:51:18 -0500

    Port Google Sign in Modal functionality in new Accounts Preferences Tab.

--- Sample/DemoUI/DemoUI/AMAccountSettingsVC.h
+++ Sample/DemoUI/DemoUI/AMAccountSettingsVC.h
@@ -7,7 +7,11 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <GTL/GTMOAuth2WindowController.h>
+#import "GTLYouTube.h"
 
-@interface AMAccountSettingsVC : NSViewController
+@interface AMAccountSettingsVC : NSViewController {
+    @private GTMOAuth2Authentication *mAuth;
+}
 
 @end

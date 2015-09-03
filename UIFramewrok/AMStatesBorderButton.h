93ddc9bc0752d1db75973354564404f14f3886f2
Brad Phillips <brad@bradphillips.org>
2015-09-03 16:43:24 -0500

    Add new border button subclass for button with various states.

---
+++ UIFramewrok/AMStatesBorderButton.h
@@ -0,0 +1,13 @@
+//
+//  AMStatesBorderButton.h
+//  UIFramework
+//
+//  Created by Brad Phillips on 9/3/15.
+//  Copyright (c) 2015 Artsmesh. All rights reserved.
+//
+
+#import <UIFramework/UIFramework.h>
+
+@interface AMStatesBorderButton : AMBlueBorderButton
+
+@end

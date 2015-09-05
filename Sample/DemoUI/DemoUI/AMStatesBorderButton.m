e0a83997a623bd0874341089951f06c7af01b827
Brad Phillips <brad@bradphillips.org>
2015-09-05 18:48:44 -0500

    Reorganize new button Classes into new UI Elements folder structure.

--- Sample/DemoUI/DemoUI/AMStatesBorderButton.m
+++ Sample/DemoUI/DemoUI/AMStatesBorderButton.m
@@ -15,7 +15,15 @@
     self = [super initWithFrame:frame];
     if (self) {
         AMStatesBorderButtonViewController *vc = [[AMStatesBorderButtonViewController alloc] initWithNibName:@"AMStatesBorderButtonViewController" bundle:nil];
-
+        NSView *vcView = [vc view];
+        
+        [self setAutoresizesSubviews:TRUE];
+        vcView.autoresizingMask = NSViewWidthSizable |  NSViewHeightSizable;
+        
+        NSSize newSize = NSMakeSize(self.frame.size.width, self.frame.size.height);
+        
+        [vcView setFrameSize:newSize];
+        
         [self addSubview:[vc view]];
     }
     

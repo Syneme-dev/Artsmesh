de4fe472818827d73dea5a2b69ad400bac251159
Brad Phillips <brad@bradphillips.org>
2015-09-05 11:30:18 -0500

    Add updated AMStatesBorderButton class.

---
+++ UIFramewrok/AMStatesBorderButton.h
@@ -0,0 +1,13 @@
+//
+//  AMStatesBorderButton.h
+//  UIFramework
+//
+//  Created by Brad Phillips on 9/5/15.
+//  Copyright (c) 2015 Artsmesh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMStatesBorderButton : NSView
+
+@end

64e71cb93f2d4bdaced0dfa750f8c510735fb172
whiskyzed <whiskyzed@gmail.com>
2015-09-14 18:10:22 +0800

    draw white bars in the correct positions.

--- Sample/DemoUI/DemoUI/AMVisualView.m
+++ Sample/DemoUI/DemoUI/AMVisualView.m
@@ -10,8 +10,17 @@
 static const int Up = 5;
 static const int Bottom = 0;
 
+static NSInteger halfBarWidth = 4; //Real white bar widht is halfBarWidth*2+1;plus one for the
+//right symetric when the auxiliary line is in the middle.
+
+
 
 @implementation AMVisualView
+{
+    NSMutableArray* verticalAuxiliaryXPos;
+    NSInteger       verticalAuxiliaryCount;
+}
+
 
 - (void) drawBoundaryWithWidth : (CGFloat) width
                withHeight      : (CGFloat) height
@@ -51,6 +60,8 @@ static const int Bottom = 0;
     [self drawHorizontalAuxiliary:width YStart:midHPosition YEnd:height withCount:2];
  
     [self drawVerticalAuxiliary:height XStart:0 XEnd:width-Up withCount:4];
+    
+    [self drawWhiteBarsCount:5];
 }
 
 
@@ -79,18 +90,33 @@ static const int Bottom = 0;
 }
 
 - (void) drawVerticalAuxiliary   : (CGFloat) height
-                XStart           : (CGFloat) YStart
-                XEnd             : (CGFloat) YEnd
+                XStart           : (CGFloat) XStart
+                XEnd             : (CGFloat) XEnd
                 withCount        : (NSInteger) count
 {
-    CGFloat delta = (YEnd - YStart)/(count + 1);
-    for (int i = 0; i < count; i++) {
-        CGFloat width = YStart + delta * (i + 1);
+    CGFloat delta = (XEnd - XStart)/(count + 1);
+    
+    if (verticalAuxiliaryXPos == nil ||
+        verticalAuxiliaryCount != count) {
+        verticalAuxiliaryXPos = [[NSMutableArray alloc] init];
+        verticalAuxiliaryCount = count;
         
+        for (int i = 0; i < count; i++) {
+            CGFloat XPos = XStart + delta * (i + 1);
+            NSNumber* number = [[NSNumber alloc] initWithDouble:XPos];
+            [verticalAuxiliaryXPos insertObject:number atIndex:i];
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+ //       CGFloat width = YStart + delta * (i + 1);
+       
+        NSNumber* curNumber= [verticalAuxiliaryXPos objectAtIndex:i];
+        CGFloat curXPos  = [curNumber doubleValue];
         NSBezierPath *auxiliaryLine = [NSBezierPath bezierPath];
         
-        NSPoint startPoint = NSMakePoint(width, Bottom+2);
-        NSPoint endPoint   = NSMakePoint(width, height-Up);
+        NSPoint startPoint = NSMakePoint(curXPos, Bottom+2);
+        NSPoint endPoint   = NSMakePoint(curXPos, height-Up);
         [auxiliaryLine moveToPoint:startPoint];
         [auxiliaryLine lineToPoint:endPoint];
         auxiliaryLine.lineWidth = 1.0;
@@ -101,6 +127,62 @@ static const int Bottom = 0;
                                    alpha:1.0f]     setStroke];
         [auxiliaryLine stroke];
     }
+}
+
+
+-(NSPoint) auxiliaryPointIndex : (NSInteger) index
+                withCount      : (NSInteger) count
+{
+    if (index == 0) {
+        return NSMakePoint(halfBarWidth, 0);
+    }
+    
+    return NSMakePoint([self bounds].size.width / count * index, 0);
+}
+
+- (void) drawWhiteBarsCount : (NSInteger) count
+{
+    CGFloat width = 2*halfBarWidth + 1;
+    CGFloat height = [self bounds].size.height;
+    
+    [[NSColor colorWithCalibratedRed:(168)/255.0f
+                               green:(168)/255.0f
+                                blue:(168)/255.0f
+                               alpha:1.0f]     set];
+    //Draw first bar
+    
+    
+    NSRect firstBarRect = NSMakeRect(0, 0, width, height*0.1);
+    [[NSBezierPath bezierPathWithRect:firstBarRect] fill];
+    
+    
+    //Draw other bars
+    for (int i = 0; i < count - 1; i++) {
+        [self drawWhiteBarIndex:i withCount:count - 1];
+    }
+}
+
+- (void) drawWhiteBarIndex : (NSInteger)index
+             withCount     : (NSInteger) count
+{
+    
+    NSNumber* curNum  = [verticalAuxiliaryXPos objectAtIndex:index];
+    CGFloat   midXPos = [curNum doubleValue];
+    CGFloat   ratio   = [self ratioValue:index];
+    
+    CGFloat xPos = midXPos - halfBarWidth;
+    CGFloat yPos = 0;
+    CGFloat width = 2*halfBarWidth + 1;
+    CGFloat height = ratio* [self bounds].size.height;
+    
+    
+    NSRect barRect = NSMakeRect(xPos, yPos, width, height);
+    [[NSBezierPath bezierPathWithRect:barRect] fill];
+}
+
+- (CGFloat) ratioValue : (NSInteger) index
+{
+    return (index+2)/10.0;
 }
 
 @end

d47ac0734ca3d5aaa12073b47e736dd19a17a2a4
Brad Phillips <brad@bradphillips.org>
2016-01-26 17:20:29 -0600

    Relocate ffmpeg configs Class and set up another class for commands.

---
+++ Sample/DemoUI/DemoUI/AMFFmpeg.h
@@ -0,0 +1,13 @@
+//
+//  AMFFmpeg.h
+//  Artsmesh
+//
+//  Created by Brad Phillips on 1/26/16.
+//  Copyright © 2016 Artsmesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMFFmpeg : NSObject
+
+@end
---
+++ Sample/DemoUI/DemoUI/AMFFmpeg.m
@@ -0,0 +1,13 @@
+//
+//  AMFFmpeg.m
+//  Artsmesh
+//
+//  Created by Brad Phillips on 1/26/16.
+//  Copyright © 2016 Artsmesh. All rights reserved.
+//
+
+#import "AMFFmpeg.h"
+
+@implementation AMFFmpeg
+
+@end

fe04f56b726bd41049dc6bc6dbe1be3ca10db8db
Brad Phillips <brad@bradphillips.org>
2016-08-20 18:04:14 -0500

    Upply dynamically themed buttons to sidebar control panel.

--- Sample/DemoUI/DemoUI/Controllers/AMPanelControlBarViewController.m
+++ Sample/DemoUI/DemoUI/Controllers/AMPanelControlBarViewController.m
@@ -63,34 +63,89 @@
 
 - (void)setButtonImages:(AMTheme *)theme {
     
+    //Help/Manual Buttons
     [self.panelHelpBtn setImage:theme.imagePanelBtnManual];
     [self.panelHelpBtn setAlternateImage:theme.imagePanelBtnManualAlt];
+    [self.panelHelpBtnSidebar setImage:theme.imagePanelBtnManual];
+    [self.panelHelpBtnSidebar setAlternateImage:theme.imagePanelBtnManualAlt];
+    
+    //User Buttons
     [self.panelUserBtn setImage:theme.imagePanelBtnUser];
     [self.panelUserBtn setAlternateImage:theme.imagePanelBtnUserAlt];
+    [self.panelUserBtnSidebar setImage:theme.imagePanelBtnUser];
+    [self.panelUserBtnSidebar setAlternateImage:theme.imagePanelBtnUserAlt];
+    
+    //Group Buttons
     [self.panelGroupBtn setImage:theme.imagePanelBtnGroup];
     [self.panelGroupBtn setAlternateImage:theme.imagePanelBtnGroupAlt];
+    [self.panelGroupBtnSidebar setImage:theme.imagePanelBtnGroup];
+    [self.panelGroupBtnSidebar setAlternateImage:theme.imagePanelBtnGroupAlt];
+    
+    //Chat Buttons
     [self.panelChatBtn setImage:theme.imagePanelBtnChat];
     [self.panelChatBtn setAlternateImage:theme.imagePanelBtnChatAlt];
+    [self.panelChatBtnSidebar setImage:theme.imagePanelBtnGroup];
+    [self.panelChatBtnSidebar setAlternateImage:theme.imagePanelBtnGroupAlt];
+    
+    //Social Buttons
     [self.panelSocialBtn setImage:theme.imagePanelBtnSocial];
     [self.panelSocialBtn setAlternateImage:theme.imagePanelBtnSocialAlt];
+    [self.panelSocialBtnSidebar setImage:theme.imagePanelBtnSocial];
+    [self.panelSocialBtnSidebar setAlternateImage:theme.imagePanelBtnSocialAlt];
+    
+    //Map Buttons
     [self.panelMapBtn setImage:theme.imagePanelBtnMap];
     [self.panelMapBtn setAlternateImage:theme.imagePanelBtnMapAlt];
+    [self.panelMapBtnSidebar setImage:theme.imagePanelBtnMap];
+    [self.panelMapBtnSidebar setAlternateImage:theme.imagePanelBtnMapAlt];
+    
+    //Route Buttons
     [self.panelRouteBtn setImage:theme.imagePanelBtnRoute];
     [self.panelRouteBtn setAlternateImage:theme.imagePanelBtnRouteAlt];
+    [self.panelRouteBtnSidebar setImage:theme.imagePanelBtnRoute];
+    [self.panelRouteBtnSidebar setAlternateImage:theme.imagePanelBtnRouteAlt];
+    
+    //Video Buttons
     [self.panelVideoBtn setImage:theme.imagePanelBtnVideo];
     [self.panelVideoBtn setAlternateImage:theme.imagePanelBtnVideoAlt];
+    [self.panelVideoBtnSidebar setImage:theme.imagePanelBtnVideo];
+    [self.panelVideoBtnSidebar setAlternateImage:theme.imagePanelBtnVideoAlt];
+    
+    //Music Buttons
     [self.panelMusicBtn setImage:theme.imagePanelBtnMusic];
     [self.panelMusicBtn setAlternateImage:theme.imagePanelBtnMusicAlt];
+    [self.panelMusicBtnSidebar setImage:theme.imagePanelBtnMusic];
+    [self.panelMusicBtnSidebar setAlternateImage:theme.imagePanelBtnMusicAlt];
+    
+    //Clock Buttons
     [self.panelClockBtn setImage:theme.imagePanelBtnClock];
     [self.panelClockBtn setAlternateImage:theme.imagePanelBtnClockAlt];
+    [self.panelClockBtnSidebar setImage:theme.imagePanelBtnClock];
+    [self.panelClockBtnSidebar setAlternateImage:theme.imagePanelBtnClockAlt];
+    
+    //OSC Buttons
     [self.panelOSCBtn setImage:theme.imagePanelBtnOSC];
     [self.panelOSCBtn setAlternateImage:theme.imagePanelBtnOSCAlt];
+    [self.panelOSCBtnSidebar setImage:theme.imagePanelBtnOSC];
+    [self.panelOSCBtnSidebar setAlternateImage:theme.imagePanelBtnOSCAlt];
+    
+    //Settings Buttons
     [self.panelSettingsBtn setImage:theme.imagePanelBtnSettings];
     [self.panelSettingsBtn setAlternateImage:theme.imagePanelBtnSettingsAlt];
+    [self.panelSettingsBtnSidebar setImage:theme.imagePanelBtnSettings];
+    [self.panelSettingsBtnSidebar setAlternateImage:theme.imagePanelBtnSettingsAlt];
+    
+    //Terminal Buttons
     [self.panelTerminalBtn setImage:theme.imagePanelBtnTerminal];
     [self.panelTerminalBtn setAlternateImage:theme.imagePanelBtnTerminalAlt];
+    [self.panelTerminalBtnSidebar setImage:theme.imagePanelBtnTerminal];
+    [self.panelTerminalBtnSidebar setAlternateImage:theme.imagePanelBtnTerminalAlt];
+    
+    //Broadcast Buttons
     [self.panelBroadcastBtn setImage:theme.imagePanelBtnBroadcast];
     [self.panelBroadcastBtn setAlternateImage:theme.imagePanelBtnBroadcastAlt];
+    [self.panelBroadcastBtnSidebar setImage:theme.imagePanelBtnBroadcast];
+    [self.panelBroadcastBtnSidebar setAlternateImage:theme.imagePanelBtnBroadcastAlt];
 }
 
 - (void) changeTheme:(NSNotification *) notification {

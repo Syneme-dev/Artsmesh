e6def86926bfdbd62857486a91e798e1c76fc7c3
whiskyzed <whiskyzed@gmail.com>
2023-04-09 14:53:38 +0800

    Local server and OSC fix bugs

--- AMOSCGroups/AMOSCGroups/AMOSCMonitor.mm
+++ AMOSCGroups/AMOSCGroups/AMOSCMonitor.mm
@@ -111,7 +111,7 @@ protected:
                 if(arg->IsBlob()){
                     
                     const void *data = NULL;
-                    long size = -1;
+                    int size = -1;
                     arg->AsBlob(data, size);
                     
                     if(data != NULL && size > 0){

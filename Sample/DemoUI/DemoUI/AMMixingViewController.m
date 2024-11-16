ddeeee3438fe63bf003a36d7396985ce5bfb4a31
whiskyzed <whiskyzed@gmail.com>
2024-11-16 16:38:28 +0800

    new

--- Sample/DemoUI/DemoUI/AMMixingViewController.m
+++ Sample/DemoUI/DemoUI/AMMixingViewController.m
@@ -9,8 +9,8 @@
 #import "AMMixingViewController.h"
 #import <UIFramework/AMButtonHandler.h>
 #import "AMAudio/AMAudio.h"
-#import "AMVideo.h"
-#import "AMVideoMixerViewController.h"
+//#import "AMVideo.h"
+
 
 @interface AMMixingViewController ()
 
@@ -19,7 +19,6 @@
 @implementation AMMixingViewController
 {
      NSViewController* _audioMixerViewController;
-     AMVideoMixerViewController* _videoMixerViewController;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -34,11 +33,10 @@
 -(void)registerTabButtons{
     super.tabs=self.tabs;
     self.tabButtons =[[NSMutableArray alloc]init];
-    [self.tabButtons addObject:self.videoTab];
     [self.tabButtons addObject:self.audioTab];
-    self.showingTabsCount=2;
-    [AMButtonHandler changeTabTextColor:self.videoTab toColor:UI_Color_blue];
-    [self onVideoTabClick:self.videoTab];
+    self.showingTabsCount   = 1;
+    [AMButtonHandler changeTabTextColor:self.audioTab toColor:UI_Color_blue];
+    [self onAudioTabClick:self.audioTab];
 }
 
 - (IBAction)onAudioTabClick:(id)sender {
@@ -46,20 +44,16 @@
     [self.tabs selectTabViewItemAtIndex:1];
 }
 
-- (IBAction)onVideoTabClick:(id)sender {
-    [self pushDownButton:self.videoTab];
-    [self.tabs selectTabViewItemAtIndex:0];
-}
+
 
 -(void)awakeFromNib
 {
     [super awakeFromNib];
     [self loadTabViews];
-    [AMButtonHandler changeTabTextColor:self.videoTab toColor:UI_Color_blue];
+
     [AMButtonHandler changeTabTextColor:self.audioTab toColor:UI_Color_blue];
     
     [self pushDownButton:self.audioTab];
-    [self pushDownButton:self.videoTab];
 }
 
 -(void)viewWillDisappear
@@ -75,8 +69,6 @@
         NSView* view = item.view;
         if ([view.identifier isEqualTo:@"Audio Mixer"]) {
             [self loadAudioMixerView: view];
-        } else if ([view.identifier isEqualTo:@"Video Mixer"]) {
-            [self loadVideoMixerView:view];
         }
     }
 }
@@ -102,30 +94,6 @@
                                                  options:0
                                                  metrics:nil
                                                    views:views]];
-    }
-}
-
--(void)loadVideoMixerView:(NSView*)tabView
-{
-    _videoMixerViewController = (AMVideoMixerViewController*)[[AMVideo sharedInstance] getMixerUI];
-    if (_videoMixerViewController) {
-        NSView* contentView = _videoMixerViewController.view;
-        //contentView.frame = NSMakeRect(0, 0, 800, 600);
-        [tabView addSubview:contentView];
-        
-        [contentView setTranslatesAutoresizingMaskIntoConstraints:NO];
-        NSDictionary *views = NSDictionaryOfVariableBindings(contentView);
-        [tabView addConstraints:
-         [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[contentView]-0-|"
-                                                 options:0
-                                                 metrics:nil
-                                                   views:views]];
-        [tabView addConstraints:
-         [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[contentView]-0-|"
-                                                 options:0
-                                                 metrics:nil
-                                                   views:views]];
-     
     }
 }
 
